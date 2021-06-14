bd58fec1b9e095a7cf77b60342d8cdedc6b6a4f7
CarlosAmorim94 <carlos.av.amorim@gmail.com>
2021-06-13 21:53:21 -0300

    Alteração

--- Exercícios Python/ex046_Contagem regressiva.py
+++ Exercícios Python/ex046_Contagem regressiva.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 n = int(input('Digite um número para contagem regressiva: '))
-for cont in range(10, -1, -1):
+for cont in range(n, -1, -1):
     sleep(1)
     print(cont)
 print('ACABOU!!')
