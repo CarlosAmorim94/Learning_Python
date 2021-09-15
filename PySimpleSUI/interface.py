7578f6cb545e9fc3d1d7b487c7b6e4fe1e56fece
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-09-14 21:44:21 -0300

    Update interface.py

--- PySimpleSUI/interface.py
+++ PySimpleSUI/interface.py
@@ -5,10 +5,26 @@ import PySimpleGUI as sg
 sg.theme('Reddit')
 #Layout
 layout = [
-    [sg.Text('Digite um número:')],
-    [sg.Input()],
-    [sg.OK()],
+    [sg.Text('Digite seu nome:')],
+    [sg.Input(key='nome')],
+    [sg.Text('Digite sua idade:')],
+    [sg.Input(key='idade')],
+    [sg.OK(), sg.Cancel(), sg.Button('Enviar dados', key='enviar_dados'),sg.Text(key='acesso', size=(20,1))],
 ]
 #Janela
 janela = sg.Window('Teste', layout)
 #Leitura dos valores da tela
+while True:
+    evento, valores = janela.Read()
+    if evento == sg.WIN_CLOSED:
+        sg.popup('Obrigado por utilizar o sistema!\nBy: Carlos Vezza' , custom_text=('Continuar' , 'Sair'))
+        break;
+    if evento == 'OK':
+        print('O seu nome é {} e sua idade é {}'.format(valores['nome'], valores['idade']))
+        if int(valores['idade']) >= 18 :
+            janela['acesso'].update('Acesso Concedido')
+        else:
+            janela['acesso'].update('Acesso Negado')
+            janela['enviar_dados'].update(disabled=True)
+            janela['nome'].update(text_color='red')
+janela.close()
